a37bc62ba8646a680d633573436e71d198531799
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2021-02-11 20:55:41 +0600

    Sign Up Screen is added

--- PetBook/src/navigation/AuthStackScreen.js
+++ PetBook/src/navigation/AuthStackScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack"
 
-import SigninScreen from "../screens/AuthScreens/SignInScreen"
+import SignInScreen from "../screens/AuthScreens/SignInScreen"
+import SignUpScreen from "../screens/AuthScreens/SignUpScreen"
 
 
 const AuthStack = createStackNavigator();
@@ -9,7 +10,8 @@ const AuthStack = createStackNavigator();
 const AuthStackScreen = () => {
   return (
     <AuthStack.Navigator initialRouteName="Sign In">
-      <AuthStack.Screen name="Sign In" component={SigninScreen} options={{ headerShown: false }} />
+      <AuthStack.Screen name="Sign In" component={SignInScreen} options={{ headerShown: false }} />
+      <AuthStack.Screen name="Sign Up" component={SignUpScreen} options={{ headerShown: false }} />
     </AuthStack.Navigator>
   )
 }
--- PetBook/src/screens/AuthScreens/SignUpScreen.js
+++ PetBook/src/screens/AuthScreens/SignUpScreen.js
@@ -1,45 +1,59 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Input, Button, Card } from "react-native-elements"
+import { Entypo, FontAwesome5, Octicons, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
-import { Entypo, FontAwesome5, MaterialCommunityIcons, Octicons } from '@expo/vector-icons';
 
-const SignInScreen = (props) => {
+const SignUpScreen = (props) => {
     return (
         <View style={styles.viewStyle}>
-            <Card containerStyle={styles.cardStyle}>
-                <Card.Title style={styles.textStyle}> Welcome to PetBook</Card.Title>
+            <Card containerStyle={styles.cardStyle}><Card.Title style={styles.textStyle}> Free Sign Up in PetBook</Card.Title>
                 <Card.Divider />
+                <Input
+                    placeholder=' Name'
+                    leftIcon={<Ionicons name="ios-person" size={24} color="black" />}
+                />
                 <Input
                     placeholder=" Email Address"
                     leftIcon={<Entypo name="mail-with-circle" size={24} color="black" />}
                 />
-
+                <Input
+                    placeholder=' Contact No'
+                    leftIcon={<Entypo name="mobile" size={24} color="black" />}
+                />
                 <Input
                     placeholder=' Password'
                     leftIcon={<FontAwesome5 name="lock" size={24} color="black" />}
+                    secureTextEntry={true}
+                />
+                <Input
+                    placeholder=' Confirm Password'
+                    leftIcon={<FontAwesome5 name="lock" size={24} color="black" />}
+                    secureTextEntry={true}
                 />
                 <Button
-                    title=' Sign In!'
+                    title=' Sign Up!'
                     icon={<MaterialCommunityIcons name="location-enter" size={24} color="white" />}
                     type='solid'
                     buttonStyle={styles.solidButtonStyle}
                     onPress={function () {
-                        console.log("Sign In Button is clicked!")
+                        console.log("Sign Up Button is clicked!")
                     }}
                 />
+                <Card.Divider />
                 <Button
-                    title=" Don't have an account? Sign Up!"
+                    title=" Already have an account? Sign In!"
                     icon={<Octicons name="person" size={24} color="#1e3d59" />}
                     titleStyle={styles.clearButtonStyle}
                     type="clear"
                     onPress={function () {
-                        console.log("Return to Sign Up Button is clicked!")
-                        props.navigation.navigate("Sign Up")
+                        console.log("Return to Sign In Button is clicked!")
+                        props.navigation.navigate("Sign In")
                     }}
                 />
+
             </Card>
-        </View >
+        </View>
     )
 }
 
@@ -72,4 +86,4 @@ const styles = StyleSheet.create(
     }
 )
 
-export default SignInScreen;
+export default SignUpScreen;
