79065c3d119216f57e923878ec0c4aab11198b59
Maysha Afrin Munia <mayshaafrin@iut-dhaka.edu>
2021-02-12 14:11:33 +0600

    vet

---
+++ PetBook/src/components/vetCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from "react-native";
+import { Card, Button, Text, Avatar } from "react-native-elements";
+import { AntDesign } from "@expo/vector-icons";
+
+const VetCard = (props) => {
+  return 
+  (
+        <View> 
+             <Text style={{ fontStyle: "italic" }}> {props.title}</Text>
+        </View>
+  );
+};
+
+export default VetCard;
