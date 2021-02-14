dbc492bf6c78e8f65432f77049960cb688630e37
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2021-02-14 20:16:41 +0600

    Profile Card is added

--- PetBook/src/screens/PostDetailsScreen.js
+++ PetBook/src/screens/PostDetailsScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import AppHeader from "../components/AppHeader"
+import PostCard from "../components/PostCard"
 
 const PostDetailsScreen = (props) => {
     return (
         <View>
             <AppHeader/>
+            <PostCard/>
             <Text style={styles.textStyle}> Welcome to Post Details Screen</Text>
         </View>
     )
